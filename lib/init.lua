170cb78c211a637c16774ea02927b3f81bdb6e8b
Justin van der Leij <justinvanderleij@gmail.com>
2018-04-29 11:21:23 +0200

    Added version, description, license

--- lib/init.lua
+++ lib/init.lua
@@ -2,7 +2,30 @@ local PATH = (...):gsub('%.init$', '')
 
 local Type = require(PATH..".type")
 
-local Concord = {}
+local Concord = {
+   _VERSION     = "1.0",
+   _DESCRIPTION = "A feature-complete ECS library",
+   _LICENCE     = [[
+      MIT LICENSE
+      Copyright (c) 2018 Justin van der Leij / Tjakka5
+      Permission is hereby granted, free of charge, to any person obtaining a
+      copy of this software and associated documentation files (the
+      "Software"), to deal in the Software without restriction, including
+      without limitation the rights to use, copy, modify, merge, publish,
+      distribute, sublicense, and/or sell copies of the Software, and to
+      permit persons to whom the Software is furnished to do so, subject to
+      the following conditions:
+      The above copyright notice and this permission notice shall be included
+      in all copies or substantial portions of the Software.
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+      OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+      IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+      CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+      TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   ]]
+}
 
 --- Initializes the library with some optional settings
 -- @param settings Table of settings: {
