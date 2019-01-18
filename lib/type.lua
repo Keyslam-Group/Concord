54fbb724ab93f34b03e4bb5144337f9a43a3f70c
Brandon Blanker Lim-it <flamendless8@gmail.com>
2019-01-18 10:25:53 +0800

    Add docs to Type

--- lib/type.lua
+++ lib/type.lua
@@ -1,17 +1,31 @@
+--- Type
+
 local Type = {}
 
+--- Check if 't' is a component
+-- @param t A table/object to be checked
+-- @return a boolean `true` if it `t` is a component, otherwise false
 function Type.isComponent(t)
    return type(t) == "table" and t.__isComponent
 end
 
+--- Check if 't' is an entity
+-- @param t A table/object to be checked
+-- @return a boolean `true` if it `t` is an entity, otherwise false
 function Type.isEntity(t)
    return type(t) == "table" and t.__isEntity
 end
 
+--- Check if 't' is a system
+-- @param t A table/object to be checked
+-- @return a boolean `true` if it `t` is a system, otherwise false
 function Type.isSystem(t)
    return type(t) == "table" and t.__isSystem
 end
 
+--- Check if 't' is an instance
+-- @param t A table/object to be checked
+-- @return a boolean `true` if it `t` is an instance, otherwise false
 function Type.isInstance(t)
    return type(t) == "table" and t.__isInstance
 end
